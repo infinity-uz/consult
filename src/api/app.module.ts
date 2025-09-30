fb615526ddf790d15ef64aa925c4ab6933394c45
Dinmuhammad Qosimov <qosimovmirjalol829@gmail.com>
2025-09-30 18:35:36 +0500

    prisma service and module

--- src/api/app.module.ts
+++ src/api/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AdminModule } from './user/admin/admin.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from 'src/core/prisma.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    PrismaModule,
     AdminModule,
   ],
   controllers: [],
